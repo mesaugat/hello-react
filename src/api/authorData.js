040f3fefa228130dfa443738bc78129cc775e342
Saugat Acharya <mesaugat@gmail.com>
2016-06-28 17:36:01 +0545

    Initial react basics

---
+++ src/api/authorData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    authors: [
+        {
+            id: 'acharya-saugat',
+            firstName: 'Saugat',
+            lastName: 'Acharya'
+        },
+        {
+            id: 'uku-don',
+            firstName: 'Uku',
+            lastName: 'Don'
+        },
+        {
+            id: 'hello',
+            firstName: 'Hello',
+            lastName: 'Kori'
+        }
+    ]
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
 $ = jQuery = require('jquery');
 
-var App = console.log('Hello world from Browserify');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
+var Authors = require('./components/authors/authorPage');
 
-module.exports = App;
+(function(win) {
+    "use strict";
+
+    var App = React.createClass({
+        render: function(){
+            var Child;
+
+            switch (this.props.route) {
+                case 'about': Child = About; break;
+                case 'authors': Child = Authors; break;
+                default: Child = Home;
+            }
+
+            return (
+                <div>
+                    <Child />
+                </div>
+            );
+        }
+    });
+
+    function render() {
+        var route = win.location.hash.substr(1);
+        React.render(<App route={route} />, document.getElementById('app'));
+    }
+
+    win.addEventListener('hashchange', render);
+    render();
+
+})(window);
