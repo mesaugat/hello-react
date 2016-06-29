66153200db11500560d787485d430fcf07f65bb4
Saugat Acharya <mesaugat@gmail.com>
2016-06-29 17:55:24 +0545

    Implement react-router

---
+++ src/components/notFoundPage.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var React = require('react');
+var Link = require('react-router').Link;
+
+var NotFoundPage = React.createClass({
+    render: function() {
+        return (
+            <div className="container">
+                <div className="jumbotron">
+                    <h1>Page Not Found</h1>
+                    <p>Whoops! Something went south.</p>
+                    <p><Link to="app">Back to Home</Link></p>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = NotFoundPage;
