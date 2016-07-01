b48f707805067be8874f8cf42268bee4eaaed1f3
Saugat Acharya <mesaugat@gmail.com>
2016-07-01 16:59:18 +0545

    Finish form processing

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -4,13 +4,20 @@ var React = require('react');
 var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({
+    propTypes: {
+        error: React.PropTypes.object,
+        onSave: React.PropTypes.func.isRequired,
+        author: React.PropTypes.object.isRequired,
+        onChange: React.PropTypes.func.isRequired
+    },
+
     render: function() {
         return (
             <form className="jumbotron">
                 <h2>Add Author</h2>
-                <Input name="firstName" label="First Name" value={this.props.author.firstName} onChange={this.props.onChange} />
-                <Input name="lastName" label="Last Name" value={this.props.author.lastName} onChange={this.props.onChange} />
-                <button type="submit" className="btn btn-default" onClick={this.props.onSave}>Add</button>
+                <Input name="firstName" label="First Name" value={this.props.author.firstName} onChange={this.props.onChange} error={this.props.errors.firstName} />
+                <Input name="lastName" label="Last Name" value={this.props.author.lastName} onChange={this.props.onChange} error={this.props.errors.lastName} />
+                <button type="submit" className="btn btn-default" onClick={this.props.onSave}>Save</button>
             </form>
         );
     }
